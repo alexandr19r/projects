8d29dd852251e2c8550a3e6887e295baf9d2603c
aleksandr.19r <alexandr.19r@gmail.com>
2026-06-10 20:23:37 +0200

    Контроль ошибки ядра

--- appmgmt/appmgmt.sh
+++ appmgmt/appmgmt.sh
@@ -6,6 +6,7 @@
 # shellcheck source=../lib/core.sh
 if ! source "$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")/lib/core.sh" 2>/dev/null; then
     printf "\033[0;31m[FATAL]\033[0m Ядро системы не найдено в корне проекта..\n" >&2
+    printf "\033[0;31m[FATAL]\033[0m $(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")/lib/core.sh" >&2
     exit 1
 fi
 
