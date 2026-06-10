182532d50f316bf54bb266d8ce50b4fa49f64a57
aleksandr.19r <alexandr.19r@gmail.com>
2026-06-10 21:02:14 +0200

    Контроль ошибки ядра

--- appmgmt/appmgmt.sh
+++ appmgmt/appmgmt.sh
@@ -6,12 +6,12 @@
 # shellcheck source=../lib/core.sh
 # Вычисляем абсолютный путь к КОРНЮ проекта (на уровень выше папки скрипта)
 BASE_DIR="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")"
-BASE_DIR1="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+BASE_DIR1="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")/lib/core.sh"
 echo "Значение BASE_DIR  : ${BASE_DIR} - 1"
 # Гарантированный вывод в терминал (перенаправляем в stderr, чтобы точно увидеть)
 printf "\033[0;34m[DEBUG]\033[0m Значение BASE_DIR: '%s'\n" "${BASE_DIR1}" >&2
 
-if ! source "$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")/lib/core.sh" 2>/dev/null; then
+if ! source "$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")/lib/core.sh" 2>/dev/null; then
     printf "\033[0;31m[FATAL]\033[0m Ядро системы не найдено в корне проекта..\n" >&2
     exit 1
 fi
